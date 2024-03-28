0378285d65c939fcc01dff2c14cdd585655c8d03
Tom Kuijer <t.kuijer@ifresh.nl>
2024-03-28 16:14:46 +0100

    Remove composer lock file, clean and add tests

--- tests/EnumTranslatorTest.php
+++ tests/EnumTranslatorTest.php
@@ -4,18 +4,21 @@ use IFresh\EnumTranslations\EnumTranslator;
 use Illuminate\Contracts\Translation\Translator;
 use Tests\Standin\TestEnum;
 
+beforeEach(function() {
+    $this->translatorMock = Mockery::mock(Translator::class);
+
+    $this->enumTranslator = new EnumTranslator($this->translatorMock);
+});
+
 it('returns translations for all enum values', function () {
-    $translator = Mockery::mock(Translator::class);
-    $translator->shouldReceive('get')
+    $this->translatorMock->shouldReceive('get')
         ->with('enums.test-enum.value_1')
         ->andReturn('Value 1');
-    $translator->shouldReceive('get')
+    $this->translatorMock->shouldReceive('get')
         ->with('enums.test-enum.value_2')
         ->andReturn('Value 2');
 
-    $enumTranslator = new EnumTranslator($translator);
-
-    $result = $enumTranslator->translate(TestEnum::class);
+    $result = $this->enumTranslator->translate(TestEnum::class);
 
     expect($result)
         ->toBeArray()
@@ -26,15 +29,33 @@ it('returns translations for all enum values', function () {
 });
 
 it('returns a translation for a single enum value', function () {
-    $translator = Mockery::mock(Translator::class);
-    $translator->shouldReceive('get')
+    $this->translatorMock->shouldReceive('get')
         ->with('enums.test-enum.value_1')
         ->andReturn('Value 1');
 
-    $enumTranslator = new EnumTranslator($translator);
-
-    $result = $enumTranslator->translateValue(TestEnum::class, TestEnum::Value1);
+    $result = $this->enumTranslator->translateValue(TestEnum::class, TestEnum::Value1);
 
     expect($result)
         ->toBe('Value 1');
 });
+
+it('returns an empty string when no backing value is given', function () {
+    $result = $this->enumTranslator->translateValue(TestEnum::class, null);
+
+    expect($result)
+        ->toBe('');
+});
+
+it('`translate` guards against invalid enums', function () {
+    $mock = new class {};
+
+    $this->enumTranslator->translate($mock::class);
+})->expectException(InvalidArgumentException::class);
+
+it('`translateValue` guards against invalid enums', function () {
+    $mock = new class {
+        const Value1 = 'value_1';
+    };
+
+    $this->enumTranslator->translateValue($mock::class, TestEnum::Value1);
+})->expectException(InvalidArgumentException::class);
