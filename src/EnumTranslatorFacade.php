6ac9bacb403f5ccd3424c1a3c45ab9552f4762b9
Tom Kuijer <t.kuijer@ifresh.nl>
2024-04-05 16:53:46 +0200

    Update Facade docblock to reflect available methods

--- src/EnumTranslatorFacade.php
+++ src/EnumTranslatorFacade.php
@@ -2,10 +2,12 @@
 
 namespace IFresh\EnumTranslations;
 
+use BackedEnum;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static array translate(string $enum, $value = null)
+ * @method static array translate(string $enum)
+ * @method static string translateValue(string $enum, BackedEnum $value)
  */
 class EnumTranslatorFacade extends Facade
 {
